6353df765c9937c695d1b1cf59e2a347e298335b
Saksham Singh Rathore <sakshamsinghrathore1304@gmail.com>
2025-06-16 01:25:55 +0530

    Add Date

--- BackEnd/src/controllers/ai.controller.js
+++ BackEnd/src/controllers/ai.controller.js
@@ -2,8 +2,8 @@ const generateContent = require("../services/ai.service");
 
 async function getReview(req, res) {
     const code = req.body.code;
-    console.log("request recieved");
-    
+    console.log(`request recieved at ${new Date()}`);
+
     if (!code) {
         return res.status(400).send("Code is required");
     }
